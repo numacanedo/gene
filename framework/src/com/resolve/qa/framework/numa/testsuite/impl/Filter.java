44beb1bbfaabc05ee54e314f2082f56074c2efa9
numacanedo <numacanedo@gmail.com>
2014-10-16 16:58:59 -0500

    Initial Version

---
+++ framework/src/com/resolve/qa/framework/numa/testsuite/impl/Filter.java
@@ -0,0 +1,56 @@
+package com.resolve.qa.framework.numa.testsuite.impl;
+
+public class Filter extends JsonAbstract{
+	public static enum CONDITION {
+		EQUALS, NOTEQUALS, CONTAINS, NOTCONTAINS, STARTSWITH, NOTSTARTSWITH, ENDWITH, NOTENDWITH, ON, BEFORE, AFTER
+	}
+	
+	private String field;
+	private String type;
+	private CONDITION condition;
+	private String value;
+	
+	public Filter() {
+		super();
+	}
+	
+	public Filter(String field, String type, CONDITION condition, String value) {
+		super();
+		this.field = field;
+		this.type = type;
+		this.condition = condition;
+		this.value = value;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public CONDITION getCondition() {
+		return condition;
+	}
+
+	public void setCondition(CONDITION condition) {
+		this.condition = condition;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
